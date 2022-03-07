60c70980783db34a28547eed441dabc41327d9b0
Jesse <jesserubytech@gmail.com>
2022-03-07 21:59:23 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
-const { defineConfig } = require('@vue/cli-service');
+// const { defineConfig } = require('@vue/cli-service');
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+// });
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hexschool-vue-week-6-3/' //儲存庫名稱
+    : '/'
+}
