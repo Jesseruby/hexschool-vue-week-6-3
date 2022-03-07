c55a287d6bac64d4b13b12bedbcaafaa024694e6
Jesseruby <47548372+Jesseruby@users.noreply.github.com>
2022-03-07 22:29:45 +0800

    Delete dist directory

--- dist/js/676.be897e8d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkweek6_1"]=self["webpackChunkweek6_1"]||[]).push([[676],{676:function(e,n,t){t.r(n),t.d(n,{default:function(){return k}});var r=t(252);const c={class:"text-center mt-3"};function u(e,n){return(0,r.wg)(),(0,r.iD)("h1",c,"後台優惠卷")}var s=t(744);const a={},f=(0,s.Z)(a,[["render",u]]);var k=f}}]);
-//# sourceMappingURL=676.be897e8d.js.map
